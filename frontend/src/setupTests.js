e4443360ea2a24eea0347f7537f85fa009c9f3b1
Mathew <muckele7@gmail.com>
2026-02-20 13:49:40 -0800

    validate end-to-end flows and fix stale bar ordering for market/recommendation data

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,23 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// CRA/Jest does not transpile axios ESM in node_modules in this setup.
+// Provide a lightweight mock for component tests.
+jest.mock('axios', () => {
+  const mockAxios = {
+    get: jest.fn(() => Promise.resolve({ data: {} })),
+    post: jest.fn(() => Promise.resolve({ data: {} })),
+    put: jest.fn(() => Promise.resolve({ data: {} })),
+    patch: jest.fn(() => Promise.resolve({ data: {} })),
+    delete: jest.fn(() => Promise.resolve({ data: {} })),
+    create: jest.fn(),
+    defaults: { headers: { common: {} } }
+  };
+  mockAxios.create.mockReturnValue(mockAxios);
+  return {
+    __esModule: true,
+    default: mockAxios,
+    ...mockAxios
+  };
+});
